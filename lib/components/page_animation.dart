e7373b45b51837e5af7d8067aa7e92de7d6149cd
Nguyen Marc <nguyen_marc@live.fr>
2019-04-15 12:41:53 +0200

    Fade Route Transition

--- lib/components/page_animation.dart
+++ lib/components/page_animation.dart
@@ -20,3 +20,18 @@ class SlideRightRoute extends PageRouteBuilder {
                   child: child,
                 ));
 }
+
+class FadeRoute<T> extends MaterialPageRoute<T> {
+  FadeRoute({WidgetBuilder builder, RouteSettings settings})
+      : super(builder: builder, settings: settings);
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    if (settings.isInitialRoute) return child;
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+}
