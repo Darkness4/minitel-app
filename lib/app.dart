86276442743709d5f82f1f251dd4afbc2a96ad46
nguyen_marc <nguyen_marc@live.fr>
2019-04-18 03:03:07 +0200

    Fade Route Transition (#29)

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'localizations.dart';
+import 'components/page_animation.dart';
 import 'pages/calendar.dart';
 import 'pages/docs.dart';
 import 'pages/maps.dart';
@@ -20,6 +21,47 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
       ],
       onGenerateTitle: (BuildContext context) => AppLoc.of(context).title,
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case '/':
+            return FadeRoute(
+              builder: (BuildContext context) =>
+                  PortalPage(title: AppLoc.of(context).titlePortalPage),
+              settings: settings,
+            );
+          case '/reporting':
+            return FadeRoute(
+              builder: (BuildContext context) =>
+                  ReportingPage(title: AppLoc.of(context).titleReportingPage),
+              settings: settings,
+            );
+          case '/docs':
+            return FadeRoute(
+              builder: (BuildContext context) => DocumentationPage(
+                  title: AppLoc.of(context).titleDocumentationPage),
+              settings: settings,
+            );
+          case '/news':
+            return FadeRoute(
+              builder: (BuildContext context) =>
+                  NewsPage(title: AppLoc.of(context).titleNewsPage),
+              settings: settings,
+            );
+          case '/maps':
+            return FadeRoute(
+              builder: (BuildContext context) =>
+                  MapsPage(title: AppLoc.of(context).titleMapsPage),
+              settings: settings,
+            );
+          case '/calendar':
+            return FadeRoute(
+              builder: (BuildContext context) =>
+                  CalendarPage(title: AppLoc.of(context).titleCalendarPage),
+              settings: settings,
+            );
+        }
+        assert(false);
+      },
       supportedLocales: [
         const Locale('en', ''), // English
         const Locale('fr', ''), // French
@@ -28,19 +70,19 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       initialRoute: '/',
-      routes: {
-        '/': (context) => PortalPage(title: AppLoc.of(context).titlePortalPage),
-        '/reporting': (context) =>
-            ReportingPage(title: AppLoc.of(context).titleReportingPage),
-        '/docs': (context) =>
-            DocumentationPage(title: AppLoc.of(context).titleDocumentationPage),
-        '/calendar': (context) =>
-            CalendarPage(title: AppLoc.of(context).titleCalendarPage),
-        '/news': (context) => NewsPage(title: AppLoc.of(context).titleNewsPage),
-        '/maps': (context) => MapsPage(title: AppLoc.of(context).titleMapsPage),
-        // '/portal': (context) =>
-        //     PortalPage(title: AppLoc.of(context).titlePortalPage),
-      },
+      // routes: {
+      //   '/': (context) => PortalPage(title: AppLoc.of(context).titlePortalPage),
+      //   '/reporting': (context) =>
+      //       ReportingPage(title: AppLoc.of(context).titleReportingPage),
+      //   '/docs': (context) =>
+      //       DocumentationPage(title: AppLoc.of(context).titleDocumentationPage),
+      //   '/calendar': (context) =>
+      //       CalendarPage(title: AppLoc.of(context).titleCalendarPage),
+      //   '/news': (context) => NewsPage(title: AppLoc.of(context).titleNewsPage),
+      //   '/maps': (context) => MapsPage(title: AppLoc.of(context).titleMapsPage),
+      //   // '/portal': (context) =>
+      //   //     PortalPage(title: AppLoc.of(context).titlePortalPage),
+      // },
     );
   }
 }
