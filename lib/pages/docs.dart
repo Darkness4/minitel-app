c1ea23680a40970b4d26565676ced2baeaf9a357
Nguyen Marc <nguyen_marc@live.fr>
2019-05-09 01:31:27 +0200

    Docs: Mise en place

--- lib/pages/docs.dart
+++ lib/pages/docs.dart
@@ -1,6 +1,11 @@
 import 'package:auto_login_flutter/components/drawer.dart';
 import 'package:auto_login_flutter/pages/docs_pages/diagnose_doc.dart';
+import 'package:auto_login_flutter/pages/docs_pages/dualboot_doc.dart';
+import 'package:auto_login_flutter/pages/docs_pages/imprimante_doc.dart';
 import 'package:auto_login_flutter/pages/docs_pages/login_doc.dart';
+import 'package:auto_login_flutter/pages/docs_pages/mail_doc.dart';
+import 'package:auto_login_flutter/pages/docs_pages/minitel_doc.dart';
+import 'package:auto_login_flutter/pages/docs_pages/virtual_machines_doc.dart';
 import 'package:flutter/material.dart';
 
 class DocumentationPage extends StatefulWidget {
@@ -24,8 +29,13 @@ class DocumentationPageState extends State<DocumentationPage> {
         child: PageView(
           controller: _controller,
           children: <Widget>[
+            MinitelDoc(),
             LoginDoc(),
             DiagnoseDoc(),
+            ImprimanteDoc(),
+            MailDoc(),
+            DualBootDoc(),
+            VirtMachineDoc(),
           ],
         ),
       ),
@@ -48,16 +58,54 @@ class DocumentationPageState extends State<DocumentationPage> {
               ),
             ),
             ListTile(
-                title: Text("Authentification"),
-                onTap: () {
-                  Navigator.pop(context);
-                  _controller.jumpToPage(0);
-                }),
+              title: Text("Minitel"),
+              onTap: () {
+                Navigator.pop(context);
+                _controller.jumpToPage(0);
+              },
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Authentification"),
+              onTap: () {
+                Navigator.pop(context);
+                _controller.jumpToPage(1);
+              },
+            ),
             ListTile(
               title: Text("Diagnostique"),
               onTap: () {
                 Navigator.pop(context);
-                _controller.jumpToPage(1);
+                _controller.jumpToPage(2);
+              },
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Configuration de l'imprimante"),
+              onTap: () {
+                Navigator.pop(context);
+                _controller.jumpToPage(0);
+              },
+            ),
+            ListTile(
+              title: Text("Importation des mails SoGo"),
+              onTap: () {
+                Navigator.pop(context);
+                _controller.jumpToPage(0);
+              },
+            ),
+            ListTile(
+              title: Text("Installer un Dual Boot"),
+              onTap: () {
+                Navigator.pop(context);
+                _controller.jumpToPage(0);
+              },
+            ),
+            ListTile(
+              title: Text("Jouer avec des VM"),
+              onTap: () {
+                Navigator.pop(context);
+                _controller.jumpToPage(0);
               },
             ),
           ],
---
+++ lib/pages/docs_pages/virtual_machines_doc.dart
@@ -0,0 +1,22 @@
+import 'package:auto_login_flutter/components/cards.dart';
+import 'package:flutter/material.dart';
+
+class VirtMachineDoc extends StatelessWidget {
+  const VirtMachineDoc({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: DocCard(
+        children: <Widget>[
+          Text(
+            "Template",
+            style: Theme.of(context).textTheme.display2,
+          ),
+        ],
+      ),
+    );
+  }
+}
