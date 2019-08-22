f5df1c529684052c871791fc9302fc67ce8f3c4a
Nguyen Marc <nguyen_marc@live.fr>
2019-08-22 22:37:24 +0200

    Gitlab CI: Integration Tests more infos

--- .ci-scripts/before_script_integration_tests.sh
+++ .ci-scripts/before_script_integration_tests.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 SCRIPTPATH=$(dirname "$(realpath "$0")")
-sdkmanager "emulator" "system-images;android-28;default;x86"
-yes | sdkmanager --licenses
-sdkmanager --update
-yes | sdkmanager --licenses
-echo no | avdmanager create avd --force -n test -k "system-images;android-28;default;x86" --abi "x86" --device "Galaxy Nexus" 
-yes | sdkmanager --licenses
 emulator -avd test -no-window -no-audio &
 /bin/bash "$SCRIPTPATH/android-wait-for-emulator"
 adb shell input keyevent 82
