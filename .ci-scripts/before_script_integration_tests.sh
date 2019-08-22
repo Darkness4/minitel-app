9f8da96039100a8ec87b6d4661537f18f30b9832
Nguyen Marc <nguyen_marc@live.fr>
2019-08-22 18:48:36 +0200

    General: Update Gradle

--- .ci-scripts/before_script_integration_tests.sh
+++ .ci-scripts/before_script_integration_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 SCRIPTPATH=$(dirname "$(realpath "$0")")
 sdkmanager "system-images;android-28;default;x86"
+yes | sdkmanager --licenses
+sdkmanager --update
+yes | sdkmanager --licenses
 echo no | avdmanager create avd --force -n test -k "system-images;android-28;default;x86" --abi "x86" --device "Galaxy Nexus" 
 emulator -avd test -no-window -no-audio &
 /bin/bash "$SCRIPTPATH/android-wait-for-emulator"
