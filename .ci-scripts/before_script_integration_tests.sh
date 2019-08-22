7c197c0b729d56c274632b2233514a994f327558
Nguyen Marc <nguyen_marc@live.fr>
2019-08-22 19:15:19 +0200

    Gitlab CI: Verbose all

--- .ci-scripts/before_script_integration_tests.sh
+++ .ci-scripts/before_script_integration_tests.sh
@@ -5,7 +5,8 @@ yes | sdkmanager --licenses
 sdkmanager --update
 yes | sdkmanager --licenses
 echo no | avdmanager create avd --force -n test -k "system-images;android-28;default;x86" --abi "x86" --device "Galaxy Nexus" 
+yes | sdkmanager --licenses
 emulator -avd test -no-window -no-audio &
 /bin/bash "$SCRIPTPATH/android-wait-for-emulator"
-adb shell input keyevent 82 &
+adb shell input keyevent 82
 adb devices
