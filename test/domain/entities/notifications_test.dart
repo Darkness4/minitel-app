8697c334a90f86efd540fda5fd055a42a01d10c4
Nguyen Marc <nguyen_marc@live.fr>
2020-02-14 20:53:46 +0100

    Unit tests: Added notifications entity test

---
+++ test/domain/entities/notifications_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:minitel_toolbox/domain/entities/notifications.dart';
+
+void main() {
+  const NotificationSettings tNotificationSettings = NotificationSettings(
+    early: Duration(minutes: 10),
+    range: Duration(days: 30),
+    enabled: true,
+  );
+
+  group('copyWith', () {
+    test(
+      'should return a valid Entity',
+      () async {
+        // act
+        final NotificationSettings result =
+            tNotificationSettings.copyWith(early: const Duration(minutes: 9));
+        // assert
+        expect(
+          result,
+          equals(
+            const NotificationSettings(
+              early: Duration(minutes: 9),
+              range: Duration(days: 30),
+              enabled: true,
+            ),
+          ),
+        );
+      },
+    );
+  });
+
+  group('toString', () {
+    test(
+      'should return a valid String',
+      () async {
+        // act
+        final String result = tNotificationSettings.toString();
+        // assert
+        expect(
+          result,
+          equals("""{
+  early: ${const Duration(minutes: 10)},
+  enabled: ${true},
+  range: ${const Duration(days: 30)},
+}"""),
+        );
+      },
+    );
+  });
+}
