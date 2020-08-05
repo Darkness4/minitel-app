c16c57f711b084c44c9bb7d8614b7edcc763869e
Nguyen Marc <nguyen_marc@live.fr>
2020-08-06 00:08:09 +0200

    [general] Updated dependencies and framework

--- lib/injection_container/injection_container.config.dart
+++ lib/injection_container/injection_container.config.dart
@@ -4,77 +4,79 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-import 'dart:async';
-
+import 'package:http/http.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
-import 'package:http/http.dart';
-import 'package:injectable/get_it_helper.dart';
+import 'package:injectable/injectable.dart';
 import 'package:ntlm/ntlm.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '../core/cookies/cookie_manager.dart';
-import '../core/datetime/datetime_manager.dart';
-import '../core/files/file_manager.dart';
-import '../core/internet_address/internet_address_manager.dart';
-import '../core/network/network_info.dart';
-import '../core/process/process_manager.dart';
+import '../presentation/cubits/agenda/agenda_cubit.dart';
+import '../presentation/cubits/portal/calendar_status/calendar_status_cubit.dart';
 import '../data/datasources/calendar_url/calendar_url_local_data_source.dart';
 import '../data/datasources/calendar_url/calendar_url_remote_data_source.dart';
+import '../domain/repositories/calendar_url_repository.dart';
+import '../data/repositories/calendar_url_repository_impl.dart';
+import '../core/cookies/cookie_manager.dart';
+import '../core/datetime/datetime_manager.dart';
+import '../domain/entities/diagnosis.dart';
+import '../presentation/cubits/reporting/diagnosis/diagnosis_cubit.dart';
 import '../data/datasources/diagnosis/diagnosis_data_source.dart';
+import '../domain/repositories/diagnosis_repository.dart';
+import '../data/repositories/diagnosis_repository_impl.dart';
+import 'injection_container.dart';
+import '../core/files/file_manager.dart';
+import '../data/datasources/github/github_local_data_source.dart';
+import '../presentation/cubits/news/github_releases/github_releases_cubit.dart';
+import '../data/datasources/github/github_remote_data_source.dart';
 import '../data/datasources/emse/icalendar_local_data_source.dart';
 import '../data/datasources/emse/icalendar_remote_data_source.dart';
-import '../data/datasources/emse/imprimante_remote_data_source.dart';
-import '../data/datasources/emse/portail_emse_remote_data_source.dart';
-import '../data/datasources/emse/stormshield_remote_data_source.dart';
-import '../data/datasources/github/github_local_data_source.dart';
-import '../data/datasources/github/github_remote_data_source.dart';
-import '../data/datasources/settings/login_settings_data_source.dart';
-import '../data/datasources/settings/notification_settings_data_source.dart';
-import '../data/datasources/slack/slack_remote_data_source.dart';
-import '../data/datasources/twitter/twitter_local_data_source.dart';
-import '../data/datasources/twitter/twitter_remote_data_source.dart';
-import '../data/datasources/zabbix/zabbix_remote_data_source.dart';
-import '../data/repositories/calendar_url_repository_impl.dart';
-import '../data/repositories/diagnosis_repository_impl.dart';
-import '../data/repositories/icalendar_repository_impl.dart';
-import '../data/repositories/login_settings_repository_impl.dart';
-import '../data/repositories/notification_settings_repository_impl.dart';
-import '../data/repositories/post_repository_impl.dart';
-import '../data/repositories/releases_repository_impl.dart';
-import '../data/repositories/zabbix_hosts_repository_impl.dart';
-import '../domain/entities/diagnosis.dart';
-import '../domain/repositories/calendar_url_repository.dart';
-import '../domain/repositories/diagnosis_repository.dart';
 import '../domain/repositories/icalendar_repository.dart';
-import '../domain/repositories/login_settings_repository.dart';
-import '../domain/repositories/notification_settings_repository.dart';
-import '../domain/repositories/post_repository.dart';
-import '../domain/repositories/releases_repository.dart';
-import '../domain/repositories/zabbix_hosts_repository.dart';
-import '../presentation/cubits/agenda/agenda_cubit.dart';
-import '../presentation/cubits/news/github_releases/github_releases_cubit.dart';
-import '../presentation/cubits/news/notification_settings/notification_settings_cubit.dart';
-import '../presentation/cubits/news/twitter_feed/twitter_feed_cubit.dart';
-import '../presentation/cubits/portal/calendar_status/calendar_status_cubit.dart';
+import '../data/repositories/icalendar_repository_impl.dart';
+import '../data/datasources/emse/imprimante_remote_data_source.dart';
 import '../presentation/cubits/portal/imprimante_status/imprimante_status_cubit.dart';
+import '../core/internet_address/internet_address_manager.dart';
+import '../data/datasources/settings/login_settings_data_source.dart';
+import '../domain/repositories/login_settings_repository.dart';
+import '../data/repositories/login_settings_repository_impl.dart';
+import '../core/network/network_info.dart';
+import '../presentation/cubits/news/notification_settings/notification_settings_cubit.dart';
+import '../data/datasources/settings/notification_settings_data_source.dart';
+import '../domain/repositories/notification_settings_repository.dart';
+import '../data/repositories/notification_settings_repository_impl.dart';
+import '../data/datasources/emse/portail_emse_remote_data_source.dart';
 import '../presentation/cubits/portal/portail_emse_status/portail_emse_status_cubit.dart';
 import '../presentation/cubits/portal/portal_cubit.dart';
 import '../presentation/cubits/portal/portal_login/portal_login_cubit.dart';
-import '../presentation/cubits/portal/stormshield_status/stormshield_status_cubit.dart';
-import '../presentation/cubits/reporting/diagnosis/diagnosis_cubit.dart';
+import '../domain/repositories/post_repository.dart';
+import '../data/repositories/post_repository_impl.dart';
+import '../core/process/process_manager.dart';
+import '../domain/repositories/releases_repository.dart';
+import '../data/repositories/releases_repository_impl.dart';
 import '../presentation/cubits/reporting/report/report_cubit.dart';
 import '../presentation/cubits/reporting/report_status/report_status_cubit.dart';
+import '../data/datasources/slack/slack_remote_data_source.dart';
+import '../data/datasources/emse/stormshield_remote_data_source.dart';
+import '../presentation/cubits/portal/stormshield_status/stormshield_status_cubit.dart';
+import '../presentation/cubits/news/twitter_feed/twitter_feed_cubit.dart';
+import '../data/datasources/twitter/twitter_local_data_source.dart';
+import '../data/datasources/twitter/twitter_remote_data_source.dart';
 import '../presentation/cubits/reporting/zabbix_hosts/zabbix_hosts_cubit.dart';
-import 'injection_container.dart';
+import '../domain/repositories/zabbix_hosts_repository.dart';
+import '../data/repositories/zabbix_hosts_repository_impl.dart';
+import '../data/datasources/zabbix/zabbix_remote_data_source.dart';
 
 /// adds generated dependencies
 /// to the provided [GetIt] instance
 
-Future<void> $initGetIt(GetIt g, {String environment}) async {
-  final gh = GetItHelper(g, environment);
+Future<GetIt> $initGetIt(
+  GetIt get, {
+  String environment,
+  EnvironmentFilter environmentFilter,
+}) async {
+  final gh = GetItHelper(get, environment, environmentFilter);
   final externalsModule = _$ExternalsModule();
   gh.lazySingleton<Client>(() => externalsModule.httpClient);
   gh.lazySingleton<Connectivity>(() => externalsModule.connectivity);
@@ -86,118 +88,119 @@ Future<void> $initGetIt(GetIt g, {String environment}) async {
       () => externalsModule.flutterLocalNotificationsPlugin);
   gh.lazySingleton<FlutterSecureStorage>(() => externalsModule.storage);
   gh.lazySingleton<GithubLocalDataSource>(
-      () => GithubLocalDataSourceImpl(fileManager: g<FileManager>()));
+      () => GithubLocalDataSourceImpl(fileManager: get<FileManager>()));
   gh.lazySingleton<GithubRemoteDataSource>(
-      () => GithubRemoteDataSourceImpl(client: g<Client>()));
+      () => GithubRemoteDataSourceImpl(client: get<Client>()));
   gh.lazySingleton<ICalendarLocalDataSource>(
-      () => ICalendarLocalDataSourceImpl(fileManager: g<FileManager>()));
+      () => ICalendarLocalDataSourceImpl(fileManager: get<FileManager>()));
   gh.lazySingleton<ICalendarRemoteDataSource>(
-      () => ICalendarRemoteDataSourceImpl(client: g<Client>()));
+      () => ICalendarRemoteDataSourceImpl(client: get<Client>()));
   gh.lazySingleton<InternetAddressManager>(() => InternetAddressManagerImpl());
   gh.lazySingleton<NTLMClient>(() => externalsModule.ntlmClient);
-  gh.lazySingleton<NetworkInfo>(() => NetworkInfoImpl(g<Connectivity>()));
+  gh.lazySingleton<NetworkInfo>(() => NetworkInfoImpl(get<Connectivity>()));
   gh.lazySingleton<NotificationDetails>(
       () => externalsModule.notificationDetails);
   gh.lazySingleton<PortailEMSERemoteDataSource>(() =>
       PortailEMSERemoteDataSourceImpl(
-          client: g<Client>(), cookieManager: g<CookieManager>()));
+          client: get<Client>(), cookieManager: get<CookieManager>()));
   gh.factory<PortailEmseStatusCubit>(
-      () => PortailEmseStatusCubit(g<PortailEMSERemoteDataSource>()));
+      () => PortailEmseStatusCubit(get<PortailEMSERemoteDataSource>()));
   gh.lazySingleton<ProcessManager>(() => ProcessManagerImpl());
   gh.lazySingleton<ReleasesRepository>(() => ReleasesRepositoryImpl(
-        localDataSource: g<GithubLocalDataSource>(),
-        remoteDataSource: g<GithubRemoteDataSource>(),
-        networkInfo: g<NetworkInfo>(),
+        localDataSource: get<GithubLocalDataSource>(),
+        remoteDataSource: get<GithubRemoteDataSource>(),
+        networkInfo: get<NetworkInfo>(),
       ));
   gh.factory<ReportStatusCubit>(() => ReportStatusCubit());
   final sharedPreferences = await externalsModule.prefs;
   gh.factory<SharedPreferences>(() => sharedPreferences);
   gh.lazySingleton<SlackRemoteDataSource>(() => SlackRemoteDataSourceImpl(
-      client: g<Client>(), dateTimeManager: g<DateTimeManager>()));
+      client: get<Client>(), dateTimeManager: get<DateTimeManager>()));
   gh.lazySingleton<StormshieldRemoteDataSource>(
-      () => StormshieldRemoteDataSourceImpl(client: g<Client>()));
+      () => StormshieldRemoteDataSourceImpl(client: get<Client>()));
   gh.factory<StormshieldStatusCubit>(
-      () => StormshieldStatusCubit(g<StormshieldRemoteDataSource>()));
+      () => StormshieldStatusCubit(get<StormshieldRemoteDataSource>()));
   gh.factory<StringBuffer>(() => externalsModule.stringBuffer);
   gh.lazySingleton<TwitterLocalDataSource>(
-      () => TwitterLocalDataSourceImpl(fileManager: g<FileManager>()));
+      () => TwitterLocalDataSourceImpl(fileManager: get<FileManager>()));
   gh.lazySingleton<TwitterRemoteDataSource>(() => TwitterRemoteDataSourceImpl(
-      client: g<Client>(), tokenBuffer: g<StringBuffer>()));
+      client: get<Client>(), tokenBuffer: get<StringBuffer>()));
   gh.lazySingleton<ZabbixRemoteDataSource>(
-      () => ZabbixRemoteDataSourceImpl(clientNoCheck: g<Client>()));
+      () => ZabbixRemoteDataSourceImpl(clientNoCheck: get<Client>()));
   gh.lazySingleton<CalendarURLLocalDataSource>(
-      () => CalendarURLLocalDataSourceImpl(prefs: g<SharedPreferences>()));
+      () => CalendarURLLocalDataSourceImpl(prefs: get<SharedPreferences>()));
   gh.lazySingleton<CalendarURLRemoteDataSource>(
-      () => CalendarURLRemoteDataSourceImpl(client: g<Client>()));
+      () => CalendarURLRemoteDataSourceImpl(client: get<Client>()));
   gh.lazySingleton<CalendarURLRepository>(() => CalendarURLRepositoryImpl(
-        localDataSource: g<CalendarURLLocalDataSource>(),
-        remoteDataSource: g<CalendarURLRemoteDataSource>(),
-        networkInfo: g<NetworkInfo>(),
+        localDataSource: get<CalendarURLLocalDataSource>(),
+        remoteDataSource: get<CalendarURLRemoteDataSource>(),
+        networkInfo: get<NetworkInfo>(),
       ));
   gh.lazySingleton<DiagnosisDataSource>(() => DiagnosisDataSourceImpl(
-        diagnosis: g<Diagnosis>(),
-        processManager: g<ProcessManager>(),
-        connectivity: g<Connectivity>(),
-        internetAddressManager: g<InternetAddressManager>(),
-        stormshieldRemoteDataSource: g<StormshieldRemoteDataSource>(),
+        diagnosis: get<Diagnosis>(),
+        processManager: get<ProcessManager>(),
+        connectivity: get<Connectivity>(),
+        internetAddressManager: get<InternetAddressManager>(),
+        stormshieldRemoteDataSource: get<StormshieldRemoteDataSource>(),
       ));
   gh.lazySingleton<DiagnosisRepository>(() => DiagnosisRepositoryImpl(
-      diagnosisDataSource: g<DiagnosisDataSource>(),
-      networkInfo: g<NetworkInfo>()));
+      diagnosisDataSource: get<DiagnosisDataSource>(),
+      networkInfo: get<NetworkInfo>()));
   gh.factory<GithubReleasesCubit>(
-      () => GithubReleasesCubit(repository: g<ReleasesRepository>()));
+      () => GithubReleasesCubit(repository: get<ReleasesRepository>()));
   gh.lazySingleton<ICalendarRepository>(() => ICalendarRepositoryImpl(
-        localDataSource: g<ICalendarLocalDataSource>(),
-        remoteDataSource: g<ICalendarRemoteDataSource>(),
-        calendarURLRepository: g<CalendarURLRepository>(),
-        networkInfo: g<NetworkInfo>(),
+        localDataSource: get<ICalendarLocalDataSource>(),
+        remoteDataSource: get<ICalendarRemoteDataSource>(),
+        calendarURLRepository: get<CalendarURLRepository>(),
+        networkInfo: get<NetworkInfo>(),
       ));
   gh.lazySingleton<ImprimanteRemoteDataSource>(() =>
       ImprimanteRemoteDataSourceImpl(
-          ntlmClient: g<NTLMClient>(), cookieManager: g<CookieManager>()));
+          ntlmClient: get<NTLMClient>(), cookieManager: get<CookieManager>()));
   gh.factory<ImprimanteStatusCubit>(
-      () => ImprimanteStatusCubit(g<ImprimanteRemoteDataSource>()));
+      () => ImprimanteStatusCubit(get<ImprimanteRemoteDataSource>()));
   gh.lazySingleton<LoginSettingsDataSource>(() => LoginSettingsDataSourceImpl(
-      prefs: g<SharedPreferences>(), storage: g<FlutterSecureStorage>()));
+      prefs: get<SharedPreferences>(), storage: get<FlutterSecureStorage>()));
   gh.lazySingleton<LoginSettingsRepository>(() => LoginSettingsRepositoryImpl(
-      localDataSource: g<LoginSettingsDataSource>()));
-  gh.lazySingleton<NotificationSettingsDataSource>(
-      () => NotificationSettingsDataSourceImpl(prefs: g<SharedPreferences>()));
+      localDataSource: get<LoginSettingsDataSource>()));
+  gh.lazySingleton<NotificationSettingsDataSource>(() =>
+      NotificationSettingsDataSourceImpl(prefs: get<SharedPreferences>()));
   gh.lazySingleton<NotificationSettingsRepository>(() =>
       NotificationSettingsRepositoryImpl(
-          localDataSource: g<NotificationSettingsDataSource>()));
-  gh.factory<PortalCubit>(
-      () => PortalCubit(loginSetingsRepository: g<LoginSettingsRepository>()));
+          localDataSource: get<NotificationSettingsDataSource>()));
+  gh.factory<PortalCubit>(() =>
+      PortalCubit(loginSetingsRepository: get<LoginSettingsRepository>()));
   gh.factory<PortalLoginCubit>(() => PortalLoginCubit(
-        stormshieldRemoteDataSource: g<StormshieldRemoteDataSource>(),
-        iCalendarRepository: g<ICalendarRepository>(),
-        imprimanteRemoteDataSource: g<ImprimanteRemoteDataSource>(),
-        portailEMSERemoteDataSource: g<PortailEMSERemoteDataSource>(),
+        stormshieldRemoteDataSource: get<StormshieldRemoteDataSource>(),
+        iCalendarRepository: get<ICalendarRepository>(),
+        imprimanteRemoteDataSource: get<ImprimanteRemoteDataSource>(),
+        portailEMSERemoteDataSource: get<PortailEMSERemoteDataSource>(),
       ));
   gh.lazySingleton<PostRepository>(() => PostRepositoryImpl(
-        localDataSource: g<TwitterLocalDataSource>(),
-        remoteDataSource: g<TwitterRemoteDataSource>(),
-        networkInfo: g<NetworkInfo>(),
+        localDataSource: get<TwitterLocalDataSource>(),
+        remoteDataSource: get<TwitterRemoteDataSource>(),
+        networkInfo: get<NetworkInfo>(),
       ));
   gh.factory<ReportCubit>(
-      () => ReportCubit(slackRemoteDataSource: g<SlackRemoteDataSource>()));
+      () => ReportCubit(slackRemoteDataSource: get<SlackRemoteDataSource>()));
   gh.factory<TwitterFeedCubit>(
-      () => TwitterFeedCubit(repository: g<PostRepository>()));
-  gh.lazySingleton<ZabbixHostsRepository>(() =>
-      ZabbixHostsRepositoryImpl(remoteDataSource: g<ZabbixRemoteDataSource>()));
+      () => TwitterFeedCubit(repository: get<PostRepository>()));
+  gh.lazySingleton<ZabbixHostsRepository>(() => ZabbixHostsRepositoryImpl(
+      remoteDataSource: get<ZabbixRemoteDataSource>()));
   gh.factory<AgendaCubit>(() => AgendaCubit(
-        flutterLocalNotificationsPlugin: g<FlutterLocalNotificationsPlugin>(),
-        notificationDetails: g<NotificationDetails>(),
-        iCalendarRepository: g<ICalendarRepository>(),
+        flutterLocalNotificationsPlugin: get<FlutterLocalNotificationsPlugin>(),
+        notificationDetails: get<NotificationDetails>(),
+        iCalendarRepository: get<ICalendarRepository>(),
       ));
   gh.factory<CalendarStatusCubit>(
-      () => CalendarStatusCubit(g<CalendarURLRepository>()));
+      () => CalendarStatusCubit(get<CalendarURLRepository>()));
   gh.factory<DiagnosisCubit>(
-      () => DiagnosisCubit(diagnosisRepository: g<DiagnosisRepository>()));
+      () => DiagnosisCubit(diagnosisRepository: get<DiagnosisRepository>()));
   gh.factory<NotificationSettingsCubit>(() => NotificationSettingsCubit(
-      repository: g<NotificationSettingsRepository>()));
+      repository: get<NotificationSettingsRepository>()));
   gh.factory<ZabbixHostsCubit>(
-      () => ZabbixHostsCubit(repository: g<ZabbixHostsRepository>()));
+      () => ZabbixHostsCubit(repository: get<ZabbixHostsRepository>()));
+  return get;
 }
 
 class _$ExternalsModule extends ExternalsModule {}
